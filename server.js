4f0df70fae9c53e8d96b42aae6d7eeaec97a5c8c
pnharrison003 <pnharrison003@gmail.com>
2025-07-19 20:10:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 function generateMockROI() {
   const base = 2 + Math.random();
-  setInterval(updateChartData, 15000);
 
  return Array.from({ length: 7 }, (_, i) =>
     parseFloat((base + i * 0.4 + Math.random()).toFixed(2))
-  );
+  );setInterval(updateChartData, 15000);
+
 }
 
 app.get('/api/roi', (req, res) => {
